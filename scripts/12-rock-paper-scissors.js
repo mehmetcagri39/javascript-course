1f3757980caccdb96a5ef832e3b9ef704ba0303d
looseygoosey <danyelricciardo3@hotmail.com>
2025-01-08 19:29:40 +0300

    advanced fonksiyonlarda add event listener ve onkeydown vb. çalışıldı. rock paper scissors artık harf ile de çalışıyor.

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -37,6 +37,36 @@ function autoPlay() {
   }
 }
 
+document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+    playGame('rock');
+  });
+
+document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+    playGame('paper');
+  });
+
+document.querySelector('.js-scissors-button')
+  .addEventListener('click', () => {
+    playGame('scissors');
+  });
+
+document.querySelector('.js-autoplay-button')
+  .addEventListener('click', () => {
+    autoPlay();
+  });
+
+  document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+      playGame('rock');
+    } else if (event.key === 'p') {
+      playGame('paper');
+    } else if (event.key === 's') {
+      playGame('scissors');
+    }
+  });
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
