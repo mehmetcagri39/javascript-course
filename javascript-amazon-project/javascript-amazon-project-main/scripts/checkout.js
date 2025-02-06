4e7d70ce8faba7a81375d3829ade01f1953da968
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2025-02-06 14:01:04 +0300

    Egzersizler bitirildi.

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity } from '../data/cart.js';
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -36,12 +36,15 @@ js-cart-item-container-${matchingProduct.id}">
         </div>
         <div class="product-quantity">
             <span>
-            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+              Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
             Update
-            <input class="quantity-input"> </input>
-            <span class="save-quantity-link link-primary">Save</span>
+            </span>
+            <input class="quantity-input js-quantity-input-${matchingProduct.id}">
+  <span class="save-quantity-link link-primary js-save-link"
+              data-product-id="${matchingProduct.id}">
+              Save
             </span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
@@ -130,8 +133,41 @@ document.querySelectorAll('.js-update-link')
     .forEach((link) => {
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
-            console.log(productId);
-        });
+            const container = document.querySelector(
+                `.js-cart-item-container-${productId}`
+              );
+              container.classList.add('is-editing-quantity');        });
     });
+
+    document.querySelectorAll('.js-save-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const quantityInput = document.querySelector(
+        `.js-quantity-input-${productId}`
+      );
+      const newQuantity = Number(quantityInput.value);
+
+      if (newQuantity < 0 || newQuantity >= 1000) {
+        alert('Quantity must be at least 0 and less than 1000');
+        return;
+      }
+      updateQuantity(productId, newQuantity);
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.remove('is-editing-quantity');
+
+      
+      const quantityLabel = document.querySelector(
+        `.js-quantity-label-${productId}`
+      );
+      quantityLabel.innerHTML = newQuantity;
+
+      updateCartQuantity();
+
+    });
+  });
 
 
