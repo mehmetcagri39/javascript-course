00fc2a37fef639a5f9fb35437f8ed164bec170e0
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2025-02-08 13:22:40 +0300

    Teslimat Seçenekleri için js kodları generate ediliyor.

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,11 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import  dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; //default export syntax
+import {deliveryOptions} from '../data/deliveryOptions.js';
+
+
 
 hello();
 
@@ -105,6 +110,40 @@ js-cart-item-container-${matchingProduct.id}">
 </div>
     `
 });
+
+function deliveryOptionsHTML() {
+    deliveryOptions.forEach((deliveryOption) => {
+        `
+            <div class="delivery-option">
+                <input type="radio"
+                class="delivery-option-input"
+                name="delivery-option-${matchingProduct.id}">
+                <div>
+                <div class="delivery-option-date">
+                    Wednesday, June 15
+                </div>
+                <div class="delivery-option-price">
+                    $4.99 - Shipping
+                </div>
+                </div>
+            </div>
+            <div class="delivery-option">
+                <input type="radio"
+                class="delivery-option-input"
+                name="delivery-option-${matchingProduct.id}">
+                <div>
+                <div class="delivery-option-date">
+                    Monday, June 13
+                </div>
+                <div class="delivery-option-price">
+                    $9.99 - Shipping
+                </div>
+                </div>
+            </div>
+
+        `
+    });
+}
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
