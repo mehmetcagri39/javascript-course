a42356f29b01a2846ddb78d5ea8c2a6e482beabb
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2025-02-11 12:32:30 +0300

    Teslimat seçenekleri interaktif yapıldı (sayfa yenilenmesi ile). Sonra bu özellik sayfa yenilenmesini gerektirmeyecek şekilde düzenlendi (Bu tekniğe MVC deniyor).

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from '../data/cart.js';
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -12,6 +12,9 @@ hello();
 const today = dayjs();
 const deliveryDate = today.add(7, 'days');
 console.log(deliveryDate.format('dddd, MMMM, D'));
+
+function renderOrderSummary() {
+
 
 let cartSummaryHTML = '';
 
@@ -107,9 +110,10 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
         const priceString = deliveryOption.priceCents === 0 ? 'FREE Shipping' : `$${formatCurrency(deliveryOption.priceCents)} - Shipping`;        
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
         html +=
         `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio"
                 ${isChecked ? 'checked' : ''}
                 class="delivery-option-input"
@@ -199,4 +203,14 @@ document.querySelectorAll('.js-save-link')
     });
   });
 
+  document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+        const {productId, deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
+        renderOrderSummary();
+    });
+  });
+}
 
+renderOrderSummary();
