2a21565581a497e45ed1fa5c0af8e31ad38aed4a
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2025-02-21 15:58:13 +0300

    Unit testler bitirildi, Intregration testlere geçildi.

---
+++ javascript-amazon-project/javascript-amazon-project-main/tests-jasmine/checkout/orderSummaryTest.js
@@ -0,0 +1,7 @@
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary";
+
+decribe('test suite: renderOrderSummary', () => {
+    it('displays the cart', () => {
+        
+    });
+});
