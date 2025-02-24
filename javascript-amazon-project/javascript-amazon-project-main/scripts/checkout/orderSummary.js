0361ebe0e811d2b62d4be57d1ac543f97bf3c116
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2025-02-24 15:07:44 +0300

    Jasmine testleri bitirildi. Birkaç eksik/düzeltme lazım.

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -53,7 +53,7 @@ cart.forEach((cartItem) => {
               data-product-id="${matchingProduct.id}">
               Save
             </span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
             Delete
             </span>
         </div>
