8a8f508773d46bc2b29488e174e17f57fd117a97
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2025-03-17 15:25:51 +0300

    Product'lar artık backend'den alınıyor. Promises ve fetch yapısının uygulanmasına başlandı.

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -2,10 +2,19 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import '../data/car.js'
+import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}); 
+//async function için, resolve en zaman yeni adıma geçileceğini kontrol ediyor.
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
--- javascript-amazon-project/javascript-amazon-project-main/tests-jasmine/checkout/orderSummaryTest.js
+++ javascript-amazon-project/javascript-amazon-project-main/tests-jasmine/checkout/orderSummaryTest.js
@@ -1,4 +1,5 @@
 import { cart, loadFromStorage } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 
 describe('test suite: renderOrderSummary', () => {
@@ -10,6 +11,12 @@ describe('test suite: renderOrderSummary', () => {
 
         const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
         const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+        beforeAll((done) => {
+            loadProducts(()=> {
+                done();
+            });
+        });
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
                 productId: productId1,
@@ -21,9 +28,11 @@ describe('test suite: renderOrderSummary', () => {
               deliveryOptionId: '2'
               }]);
         });
+
         loadFromStorage();
 
         renderOrderSummary();
+        
 
         expect(
             document.querySelectorAll('js-cart-item-container').length
