63c87fe1a04fe95878fe51114fba5b78896a5e36
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2025-03-18 13:51:56 +0300

    Product ve cart'ı çağırırken nested yapı kullanmak yerine Promise yapısı kullanıldı.

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -3,18 +3,33 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import '../data/car.js'
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-new Promise((resolve) => {
+new Promise((resolve) => { //promises let us split the code
     loadProducts(() => {
         resolve();
     });
-}); 
-//async function için, resolve en zaman yeni adıma geçileceğini kontrol ediyor.
-
-loadProducts(() => {
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+     resolve();
+    });
+  });
+}).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
+})
+//async function için, resolve en zaman yeni adıma geçileceğini kontrol ediyor.
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });  
 });
+*/
